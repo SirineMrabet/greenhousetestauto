9b338b2ffcff111ce031a38892946fb5596cb8c7
Sirine MRABET <sirine.mrabet@spectrumgroupe.fr>
2018-05-02 15:22:05 +0100

    Sign in Feature

--- src/test/java/Steps/SignIN_SignUP/SignIn.java
+++ src/test/java/Steps/SignIN_SignUP/SignIn.java
@@ -1,10 +1,19 @@
 package Steps.SignIN_SignUP;
 
 import Base.BaseUtile;
+import Helpers.ConfigGetPropertyValues;
+import Pages.SignIN_Page;
+import Pages.SignUP_Page;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.util.List;
 
 
 /**
@@ -12,44 +21,70 @@ import cucumber.api.java.en.When;
  */
 public class SignIn extends BaseUtile{
 
+    SignIN_Page PO;
+    ConfigGetPropertyValues Config;
     SignIn(BaseUtile Base){
         this.driver=Base.driver;
+        SignIN_Page PO = new SignIN_Page(Base.driver);
+        Config = new ConfigGetPropertyValues();
     }
 
     @When("^I click on the link to sign-in$")
-    public void i_click_on_the_link_to_sign_in()  {
+    public void i_click_on_the_link_to_sign_in() throws IOException {
+        PO.SignIN.click();
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL, Config.getPropValues("Config/Config.properties","url")+"signin");
 
     }
 
-    @When("^I enter SirineMrabet s@tg\\$M(\\d+)%$")
-    public void i_enter_SirineMrabet_s_tg$M(int arg1) {
-
-    }
+    @And("^I put ([^\\\"]*) ([^\\\"]*)$")
+    public void iPutLoginPassword(String login, String pass)  {
+        PO.login.sendKeys(login);
+        PO.password.sendKeys(pass);
+            }
 
     @When("^I click on sign-in button$")
     public void i_click_on_sign_in_button()  {
+        PO.submit.click();
 
     }
 
     @Then("^the current user is connected$")
-    public void the_current_user_is_connected()  {
+    public void the_current_user_is_connected() throws IOException {
+        SignIN_Page P1 = new SignIN_Page(driver);
+        String userName=  Config.getPropValues("Config/Users.properties","user1");
+
+        List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + userName + "')]"));
+        Assert.assertTrue(list.size() > 0,"Text not found!");
+        list = driver.findElements(By.xpath("//*[contains(text(),'Sign out')]"));
+        Assert.assertTrue(list.size() > 0,"Text not found!");
+        P1.Sign_out.click();
 
     }
 
     
 
     @Then("^the current user is not connected$")
-    public void the_current_user_is_not_connected()  {
+    public void the_current_user_is_not_connected() throws IOException {
+        SignIN_Page P1 = new SignIN_Page(driver);
+        String userName=  Config.getPropValues("Config/Users.properties","user1");
+
+        List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + userName + "')]"));
+        Assert.assertFalse(list.size() > 0,"Text found!");
+        list = driver.findElements(By.xpath("//*[contains(text(),'Sign out')]"));
+        Assert.assertFalse(list.size() > 0,"Text found!");
+
 
     }
 
     @Then("^an error msg is displayed indicating that the current user does no have an account$")
-    public void an_error_msg_is_displayed_indicating_that_the_current_user_does_no_have_an_account()  {
+    public void an_error_msg_is_displayed_indicating_that_the_current_user_does_no_have_an_account() throws IOException {
+        List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + Config.getPropValues("Config/msg.properties","NOTExist") + "')]"));
+        Assert.assertTrue(list.size() > 0,"Text not found!");
+
+
 
     }
 
-    @And("^I put ([^\\\"]*) ([^\\\"]*)$")
-    public void iPutLoginPassword()  {
 
-    }
 }
